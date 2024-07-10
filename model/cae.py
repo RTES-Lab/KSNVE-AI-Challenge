a395f77d6a3d22a28fbbda4893deea3bf3c0dc62
seongjae <junior209@uos.ac.kr>
2024-07-10 17:28:29 +0900

    feat: 2D STFT CNN 모델 추가

--- model/cae.py
+++ model/cae.py
@@ -35,6 +35,39 @@ class Downsample(nn.Module):
         return x
 
 
+class Downsample2D(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        ratio,
+        activation=nn.ReLU,
+        normalization=True,
+    ):
+        super(Downsample2D, self).__init__()
+        if kernel_size - ratio < 2:
+            raise ValueError("K - S must > 2")
+        padding = int((kernel_size - ratio) / 2)
+
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, ratio, padding)
+        if normalization:
+            self.bn = nn.BatchNorm2d(out_channels)
+        else:
+            self.bn = nn.Identity()
+        if activation:
+            self.act = activation()
+        else:
+            self.act = nn.Identity()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.act(x)
+
+        return x
+
+
 class Upsample(nn.Module):
     def __init__(
         self,
@@ -70,12 +103,47 @@ class Upsample(nn.Module):
         return x
 
 
+class Upsample2D(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        ratio,
+        activation=nn.ReLU,
+        normalization=True,
+    ):
+        super(Upsample2D, self).__init__()
+        if kernel_size - ratio < 2:
+            raise ValueError("K - S must > 2")
+        padding = int((kernel_size - ratio) / 2)
+
+        self.conv = nn.ConvTranspose2d(
+            in_channels, out_channels, kernel_size, ratio, padding
+        )
+        if normalization:
+            self.bn = nn.BatchNorm2d(out_channels)
+        else:
+            self.bn = nn.Identity()
+        if activation:
+            self.act = activation()
+        else:
+            self.act = nn.Identity()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.act(x)
+
+        return x
+
+
 class LSTM1DCAE(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels=2):
         super(LSTM1DCAE, self).__init__()
 
         self.encoder = nn.Sequential(
-            Downsample(4, 32, 256, 16, nn.ReLU, True),
+            Downsample(in_channels, 32, 256, 16, nn.ReLU, True),
             Downsample(32, 64, 32, 16, nn.ReLU, True),
             Downsample(64, 64, 16, 2, nn.ReLU, True),
             Downsample(64, 64, 8, 2, nn.ReLU, True),
@@ -89,7 +157,7 @@ class LSTM1DCAE(nn.Module):
             Upsample(64, 64, 8, 2, nn.ReLU, True),
             Upsample(64, 64, 16, 2, nn.ReLU, True),
             Upsample(64, 32, 32, 16, nn.ReLU, True),
-            Upsample(32, 4, 256, 16, False, False),
+            Upsample(32, in_channels, 256, 16, False, False),
         )
 
     def forward(self, x):
@@ -112,11 +180,11 @@ class LSTM1DCAE(nn.Module):
 
 
 class Simple1DCAE(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels=2, hidden_dim=32):
         super(Simple1DCAE, self).__init__()
 
         self.encoder = nn.Sequential(
-            Downsample(4, 32, 256, 16, nn.ReLU, True),
+            Downsample(in_channels, 32, 256, 16, nn.ReLU, True),
             Downsample(32, 64, 32, 16, nn.ReLU, True),
             Downsample(64, 64, 16, 2, nn.ReLU, True),
             Downsample(64, 64, 8, 2, nn.ReLU, True),
@@ -125,17 +193,17 @@ class Simple1DCAE(nn.Module):
         self.flatten = nn.Flatten()
 
         self.mlp = nn.Sequential(
-            torch.nn.Linear(1600, 32),
-            torch.nn.BatchNorm1d(32),
+            torch.nn.Linear(1600, hidden_dim),
+            torch.nn.BatchNorm1d(hidden_dim),
             nn.ReLU(),
-            torch.nn.Linear(32, 1600),
+            torch.nn.Linear(hidden_dim, 1600),
         )
 
         self.decoder = nn.Sequential(
             Upsample(64, 64, 8, 2, nn.ReLU, True),
             Upsample(64, 64, 16, 2, nn.ReLU, True),
             Upsample(64, 32, 32, 16, nn.ReLU, True),
-            Upsample(32, 4, 256, 16, False, False),
+            Upsample(32, in_channels, 256, 16, False, False),
         )
 
     def forward(self, x):
@@ -157,75 +225,38 @@ class Simple1DCAE(nn.Module):
         return x
 
 
-class Simple1DSTFTCAE(nn.Module):
-    def __init__(self):
-        super(Simple1DSTFTCAE, self).__init__()
+class Simple2DSTFTCAE(nn.Module):
+    def __init__(self, in_channels=2):
+        super(Simple2DSTFTCAE, self).__init__()
 
         self.encoder = nn.Sequential(
-            Downsample(16, 32, 32, 4, nn.ReLU, True),
-            Downsample(32, 64, 16, 4, nn.ReLU, True),
-            Downsample(64, 64, 8, 2, nn.ReLU, True),
-            Downsample(64, 64, 4, 2, nn.ReLU, True),
+            Downsample2D(in_channels, 16, 32, 2, nn.ReLU, True),
+            Downsample2D(16, 32, 16, 2, nn.ReLU, True),
+            Downsample2D(32, 64, 8, 2, nn.ReLU, True),
+            Downsample2D(64, 64, 4, 2, nn.ReLU, True),
         )
 
         self.flatten = nn.Flatten()
 
         self.mlp = nn.Sequential(
-            torch.nn.Linear(4096, 8),
-            torch.nn.BatchNorm1d(8),
-            nn.ReLU(),
-            torch.nn.Linear(8, 4096),
-        )
-
-        self.decoder = nn.Sequential(
-            Upsample(64, 64, 4, 2, nn.ReLU, True),
-            Upsample(64, 64, 8, 2, nn.ReLU, True),
-            Upsample(64, 32, 16, 4, nn.ReLU, True),
-            Upsample(32, 16, 32, 4, False, False),
-        )
-
-    def forward(self, x):
-        x = self.encoder(x)
-        x = self.flatten(x)
-        x = self.mlp(x)
-        x = x.view(-1, 64, 64)
-        x = self.decoder(x)
-
-        return x
-
-
-class Simple1DDecCAE(nn.Module):
-    def __init__(self):
-        super(Simple1DDecCAE, self).__init__()
-
-        self.encoder = nn.Sequential(
-            Downsample(6, 16, 128, 2, nn.ReLU, True),
-            Downsample(16, 32, 64, 2, nn.ReLU, True),
-            Downsample(32, 64, 32, 2, nn.ReLU, True),
-            Downsample(64, 64, 16, 2, nn.ReLU, True),
-        )
-
-        self.flatten = nn.Flatten()
-
-        self.mlp = nn.Sequential(
-            torch.nn.Linear(3200, 32),
+            torch.nn.Linear(4096, 32),
             torch.nn.BatchNorm1d(32),
             nn.ReLU(),
-            torch.nn.Linear(32, 3200),
+            torch.nn.Linear(32, 4096),
         )
 
         self.decoder = nn.Sequential(
-            Upsample(64, 64, 16, 2, nn.ReLU, True),
-            Upsample(64, 32, 32, 2, nn.ReLU, True),
-            Upsample(32, 16, 64, 2, nn.ReLU, True),
-            Upsample(16, 6, 128, 2, False, False),
+            Upsample2D(64, 64, 4, 2, nn.ReLU, True),
+            Upsample2D(64, 32, 8, 2, nn.ReLU, True),
+            Upsample2D(32, 16, 16, 2, nn.ReLU, True),
+            Upsample2D(16, in_channels, 32, 2, False, False),
         )
 
     def forward(self, x):
         x = self.encoder(x)
         x = self.flatten(x)
         x = self.mlp(x)
-        x = x.view(-1, 64, 50)
+        x = x.view(-1, 64, 8, 8)
         x = self.decoder(x)
 
         return x

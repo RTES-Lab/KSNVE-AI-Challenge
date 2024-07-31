6260a0e4252f9aa5817a15ad420a1a3c97a0571f
seongjae <junior209@uos.ac.kr>
2024-07-31 17:51:28 +0900

    feat: update main source (r.w.t. fix random seeds)

--- run_fit_dl.py
+++ run_fit_dl.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import pandas as pd
+import os
 
 import torch
 from torchvision import transforms
@@ -12,71 +13,37 @@ from train import Trainer, multiclass_roc_auc
 
 # reproducibility를 위해
 
-seed = 42
+def set_seed(seed):
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.use_deterministic_algorithms(True)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
-random.seed(seed)
-np.random.seed(seed)
-torch.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
-# torch.backends.cudnn.deterministic = True
-# torch.backends.cudnn.benchmark = False
+def train_model(result_dir,
+                model,
+                model_kwargs,
+                optim,
+                optim_kwargs,
+                loss,
+                loss_kwargs,
+                tf,
+                batch_size,
+                n_epochs,
+                n_workers,
+                df_train,
+                df_val,
+                seed,
+                verbose=True):
+    set_seed(seed)
+    train_loader = data.get_dataloader(df_train, tf, None, True, batch_size, n_workers)
+    val_loader = data.get_dataloader(df_val, tf, None, False, 1, n_workers)
 
-print("#1 Generate Dataframe...")
-
-df_train = data.generate_df("./dataset/train")
-df_val = data.generate_df("./dataset/eval")
-
-########## 훈련에 필요한 파라미터 입력 ##########
-result_dir = "./logs/test"  # 훈련 결과를 저장할 위치
-
-# 직교좌표만 쓰는경우
-# tf = transforms.Compose(
-#     [
-#         proc.NpToTensor(),
-#     ]
-# )
-
-# STFT 변환 사용할 경우
-# tf = transforms.Compose(
-#     [
-#         proc.STFT2D(),
-#         proc.NpToTensor(),
-#     ]
-# )
-
-# 데이터 전처리 시퀀스 (직교좌표 + 극좌표 사용하는 경우)
-tf = transforms.Compose(
-    [
-        proc.Polar(),
-        proc.NpToTensor(),
-    ]
-)
-
-batch_size = 16  # 배치 크기
-n_epochs = 400  # 에폭 수
-n_workers = 8  # 데이터 로딩에 사용할 코어수
-
-model = cae.Simple1DCAE  # 사용할 모델
-model_kwargs = {"in_channels": 4, "hidden_dim": 512}  # 사용할 모델에 들어갈 인자
-
-optim = torch.optim.Adam  # 사용할 최적화 알고리즘
-optim_kwargs = {"lr": 0.004}  # 사용할 최적화 알고리즘에 들어갈 인자
-
-loss = torch.nn.MSELoss  # 사용할 손실함수
-loss_kwargs = None  # 사용할 손실함수에 들어갈 인자
-
-use_md = False  # 마할라노비스 거리 사용할건지?
-##########                  ##########
-
-print("#2 Generate DataLoader...")
-
-train_loader = data.get_dataloader(df_train, tf, None, True, batch_size, n_workers)
-
-val_loader = data.get_dataloader(df_val, tf, None, False, 1, n_workers)
-
-print("#3 Train Model...")
-
-trainer = Trainer(
+    trainer = Trainer(
     model,
     model_kwargs,
     optim,
@@ -86,49 +53,53 @@ trainer = Trainer(
     train_loader,
     val_loader,
     "cuda",
-)
+    )
 
-trainer.train(n_epochs, True)
-log = trainer.val()
-trainer.save(result_dir)
-
-# 마할라노비스 거리 기반으로 prediction 다시 계산
-
-if use_md:
-    log = {"pred": [], "gt": []}
-
-    m = torch.load(f"{result_dir}/model.pt")
-
-    losses = []
-
-    for i in df_train.index:
-        with torch.no_grad():
-            x = df_train.iloc[i]["data"].astype("float32")
-            x = tf(x).unsqueeze(0).to("cuda")
-            y = m(x)
-            loss = trainer.loss_fn(x, y).item()
-            losses.append(loss)
-
-    mh_u = np.mean(np.array(losses))
-    mh_std = np.std(np.array(losses))
-
-    for i in df_val.index:
-        with torch.no_grad():
-            x = df_val.iloc[i]["data"].astype("float32")
-            x = tf(x).unsqueeze(0).to("cuda")
-            y = m(x)
-            loss = trainer.loss_fn(x, y).item()
-            mhd = abs(loss - mh_u) / mh_std
-            log["pred"].append(mhd)
-            log["gt"].append(df_val.iloc[i]["label"])
-
-    trainer.val_log = log
+    trainer.train(n_epochs, verbose)
+    log = trainer.val()
     trainer.save(result_dir)
 
-print("#4 Evaluate Model...")
+    return trainer
 
-class_aucs, total_auc = multiclass_roc_auc(np.array(log["gt"]), np.array(log["pred"]))
+if __name__ == "__main__":
+    print("#1 Generate Dataframe...")
+    df_train = data.generate_df("./dataset/train")
+    df_val = data.generate_df("./dataset/eval")
 
-for i, class_auc in enumerate(class_aucs):
-    print(f"CLASS {i}, ROC-AUC {class_auc}")
-print(f"TOTAL, ROC-AUC {total_auc}")
+    print("#2 Train LSTM CAE...")
+    train_model(
+        result_dir="./result/lstm1dcae",
+        model=cae.LSTM1DCAE,
+        model_kwargs={"in_channels": 2},
+        optim=torch.optim.Adam,
+        optim_kwargs={"lr": 0.004},
+        loss=torch.nn.MSELoss,
+        loss_kwargs=None,
+        tf=transforms.Compose([proc.NpToTensor(),]),
+        batch_size=16,
+        n_epochs=400,
+        n_workers=0,
+        df_train=df_train,
+        df_val=df_val,
+        seed=42,
+        verbose=True
+    )
+
+    print("#3 Train STFT CAE...")
+    train_model(
+        result_dir="./result/stft2dcae",
+        model=cae.Simple2DSTFTCAE,
+        model_kwargs={"in_channels": 2},
+        optim=torch.optim.Adam,
+        optim_kwargs={"lr": 0.004},
+        loss=torch.nn.MSELoss,
+        loss_kwargs=None,
+        tf=transforms.Compose([proc.STFT2D(),proc.NpToTensor(),]),
+        batch_size=16,
+        n_epochs=400,
+        n_workers=0,
+        df_train=df_train,
+        df_val=df_val,
+        seed=42,
+        verbose=True
+    )
